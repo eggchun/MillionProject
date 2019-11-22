db6fad153eb53324a1b2843c76a48963ac7f8f1f
eggchun <54836790+eggchun@users.noreply.github.com>
2019-11-22 23:06:38 +0800

    all layout

--- app/src/main/java/com/example/millionproject/MainActivity.java
+++ app/src/main/java/com/example/millionproject/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+//        final String[] theme = {"Christmas", "Hong Kong", "Anime", "Festival"};
+//        ArrayAdapter<String> themeList = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, theme);
+//        spinner.setAdapter(themeList);
     }
 
     @Override
